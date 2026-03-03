9d8b17d3c31542288b75ed642d28dd15097b7548
yasiru kodikara <yasirukodikara15@gmail.com>
2026-03-03 08:27:09 +0530

    env api changed

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -1,4 +1,4 @@
 export const environment = {
   production: true,
-  apiUrl: 'https://travelx-backend-production.up.railway.app/api' // Your future Railway URL
+  apiUrl: 'https://primrose-safari-backend-production.up.railway.app/api' // Your future Railway URL
 };
